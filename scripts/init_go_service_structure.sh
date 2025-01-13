d459d5bdc682e57bfb035bbb8835ad18df0fe3ee
Abdur Razzak <abdur.razzak@silverlakeaxis.com>
2025-01-13 17:06:26 +0800

    update go project init bash script for routes.go codes

--- scripts/init_go_service_structure.sh
+++ scripts/init_go_service_structure.sh
@@ -561,6 +561,63 @@ func InitRoutes(router *gin.Engine) {
 }
 EOF
 
+# Create a basic container.go
+    cat <<EOF > routes/routes.go
+
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+// SetupRoutes initializes all API routes grouped by category and applies appropriate middlewares.
+func SetupRoutes(
+	router *gin.Engine,
+	publicAuthController *controllers.PublicAuthController,
+	protectedAuthController *controllers.ProtectedAuthController,
+	internalAuthController *controllers.InternalAuthController,
+) {
+	// Attach exception middleware
+	router.Use(middlewares.ErrorHandler())
+
+	// Initialize Public API routes
+	initializePublicRoutes(router, publicAuthController)
+
+	// Initialize Protected API routes
+	initializeProtectedRoutes(router, protectedAuthController)
+
+	// Initialize Internal API routes
+	initializeInternalRoutes(router, internalAuthController)
+}
+
+// initializePublicRoutes sets up routes for Public APIs
+func initializePublicRoutes(router *gin.Engine, controller *controllers.PublicAuthController) {
+	publicGroup := router.Group("/v1/public/auth")
+	{
+
+	}
+}
+
+// initializeProtectedRoutes sets up routes for Protected APIs
+func initializeProtectedRoutes(router *gin.Engine, controller *controllers.ProtectedAuthController) {
+	protectedGroup := router.Group("/v1/protected/auth")
+	protectedGroup.Use(middlewares.AuthMiddleware()) // Apply JWT validation middleware
+	{
+
+	}
+}
+
+// initializeInternalRoutes sets up routes for Internal APIs
+func initializeInternalRoutes(router *gin.Engine, controller *controllers.InternalAuthController) {
+	internalGroup := router.Group("/v1/internal/auth")
+	internalGroup.Use(middlewares.BasicAuthMiddleware) // Apply Basic Auth middleware
+	{
+
+	}
+}
+
+EOF
+
     # Run go mod tidy to clean up dependencies
     go mod tidy
 }
